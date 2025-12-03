de9a071b19cd55cd99e8cfa48ac1ec123dcb672c
oskhar <muhamadoskharm@gmail.com>
2025-12-03 15:07:25 +0700

    ✨ feat(api): add endpoint create stock

--- api/nestjs/baseline/src/domain/inventory/stock/stock.module.ts
+++ api/nestjs/baseline/src/domain/inventory/stock/stock.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StockService } from './stock.service';
 import { StockController } from './stock.controller';
+import { Stock } from './entities/stock.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Stock])],
   controllers: [StockController],
   providers: [StockService],
 })
--- api/nestjs/baseline/src/domain/inventory/stock/stock.service.ts
+++ api/nestjs/baseline/src/domain/inventory/stock/stock.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import type { CreateStockDto } from './dto/create-stock.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Stock } from './entities/stock.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class StockService {
-  create(createStockDto: CreateStockDto) {
-    return 'This action adds a new stock';
+  constructor(
+    @InjectRepository(Stock)
+    private readonly stockRepository: Repository<Stock>,
+  ) {}
+
+  async create(createStockDto: CreateStockDto) {
+    const stock = this.stockRepository.create(createStockDto);
+    await this.stockRepository.save(stock);
+
+    return stock;
   }
 
-  findAll() {
-    return `This action returns all stock`;
-  }
+  findAll() {}
 
   findOne(id: number) {
     return `This action returns a #${id} stock`;
