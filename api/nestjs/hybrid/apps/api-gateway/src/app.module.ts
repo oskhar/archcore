fa463cd9d4c7fdd93a6343823a70e2a51d627848
oskhar <muhamadoskharm@gmail.com>
2026-01-22 05:11:19 +0700

    🔧 chore(config): add api gateway as orchestration

--- api/nestjs/hybrid/apps/api-gateway/src/app.module.ts
+++ api/nestjs/hybrid/apps/api-gateway/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MICROSERVICES_CLIENTS } from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: MICROSERVICES_CLIENTS.PRODUCTS_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          port: 4001,
+        },
+      },
+      {
+        name: MICROSERVICES_CLIENTS.INVENTORIES_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          port: 4002,
+        },
+      },
+      {
+        name: MICROSERVICES_CLIENTS.SALES_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          port: 4003,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
